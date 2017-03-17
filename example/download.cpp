e40547ed9526aaf09ab6929504bc1896459bd7a1
Dendelion <sultanov-57@mail.ru>
2017-03-17 11:40:26 +0300

    Update download.cpp

--- example/download.cpp
+++ example/download.cpp
@@ -5,11 +5,11 @@ void main()
    `yadisk::Client Client { "token" };
   
     // Download example.dat 
-    auto response = download("example.dat", "example.dat"); 
+    auto response = Client.download("example.dat", "example.dat"); 
 
     // Download example.dat in "/usr/local/tmp/"
-    response = download("disk:/example.dat", "/usr/local/tmp/example.dat"); 
+    response = Client.download("disk:/example.dat", "/usr/local/tmp/example.dat"); 
 
     // Download example.dat, responce join list key "name", "_embedded.items.path"
-    response = download("example.dat", "example.dat", new std::list<std::string>{"name","_embedded.items.path"});     
+    response = Client.download("example.dat", "example.dat", new std::list<std::string>{"name","_embedded.items.path"});     
 }
